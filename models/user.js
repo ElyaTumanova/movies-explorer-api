47422257e6db8967145306e2f6fcaea816717161
Elya Tumanova <elatumanova@yandex.ru>
2023-12-16 14:14:03 +0300

    update

--- models/user.js
+++ models/user.js
@@ -30,12 +30,15 @@ userSchema.statics.findUserByCredentials = function findUser(email, password, ne
     .then((user) => {
       if (!user) {
         console.log('no user');
-        return Promise.reject(new AuthError('Пользователь не найден'));
+        throw new AuthError('Пользователь не найден');
+        // return Promise.reject(new AuthError('Пользователь не найден'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new AuthError('Пользователь или пароль указаны не верно'));
+            console.log('no match');
+            throw new AuthError('Пользователь или пароль указаны не верно');
+            // return Promise.reject(new AuthError('Пользователь или пароль указаны не верно'));
           }
 
           return user; // теперь user доступен
